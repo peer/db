1c1ce03ef9876fe60809c2b1c1cc990a2029b772
Mitar <mitar.git@tnode.com>
2022-01-30 00:31:48 +0100

    Enable HTTP2.

--- cmd/search/search.go
+++ cmd/search/search.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"math/big"
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/olivere/elastic/v7"
@@ -9,6 +18,53 @@ import (
 
 	"gitlab.com/peerdb/search"
 )
+
+func genereateCertificate() tls.Certificate {
+	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		panic(err)
+	}
+
+	keyUsage := x509.KeyUsageDigitalSignature
+	notBefore := time.Now()
+	notAfter := notBefore.Add(365 * 24 * time.Hour)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		panic(err)
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"PeerDB Search"},
+		},
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		KeyUsage:              keyUsage,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IPAddresses:           []net.IP{net.ParseIP("127.0.0.1")},
+		DNSNames:              []string{"localhost"},
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		panic(err)
+	}
+
+	cert, err := x509.ParseCertificate(derBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return tls.Certificate{
+		Certificate: [][]byte{cert.Raw},
+		PrivateKey:  priv,
+		Leaf:        cert,
+	}
+}
 
 func listen(config *Config) errors.E {
 	client, err := elastic.NewClient()
@@ -25,5 +81,13 @@ func listen(config *Config) errors.E {
 	router.HandleMethodNotAllowed = true
 	router.NotFound = search.NotFound
 
-	return errors.WithStack(http.ListenAndServe(":8080", router))
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+		TLSConfig: &tls.Config{
+			Certificates: []tls.Certificate{genereateCertificate()},
+		},
+	}
+
+	return errors.WithStack(server.ListenAndServeTLS("", ""))
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.17
 require github.com/alecthomas/kong v0.2.22
 
 require (
+	github.com/golang/gddo v0.0.0-20210115222349-20d68f94ee1f
+	github.com/julienschmidt/httprouter v1.3.0
 	github.com/olivere/elastic/v7 v7.0.31
 	github.com/stretchr/testify v1.7.0
 	gitlab.com/tozd/go/mediawiki v0.4.0
@@ -25,12 +27,10 @@ require (
 	github.com/go-git/gcfg v1.5.0 // indirect
 	github.com/go-git/go-billy/v5 v5.3.1 // indirect
 	github.com/go-git/go-git/v5 v5.4.2 // indirect
-	github.com/golang/gddo v0.0.0-20210115222349-20d68f94ee1f // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
