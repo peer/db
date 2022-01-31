ddf0a503f3abf2b3fae8cf90f0e384663f845739
Mitar <mitar.git@tnode.com>
2022-01-31 02:20:03 +0100

    Reuse TLS certificate.

--- cmd/search/config.go
+++ cmd/search/config.go
@@ -7,5 +7,7 @@ import (
 // Config provides configuration.
 // It is used as configuration for Kong command-line parser as well.
 type Config struct {
-	Version kong.VersionFlag `short:"V" help:"Show program's version and exit."`
+	Version  kong.VersionFlag `short:"V" help:"Show program's version and exit."`
+	CertFile string           `short:"c" placeholder:"PATH" required:"" type:"existingfile" help:"A certificate for TLS."`
+	KeyFile  string           `short:"k" placeholder:"PATH" required:"" type:"existingfile" help:"A certificate's matching private key."`
 }
--- cmd/search/search.go
+++ cmd/search/search.go
@@ -1,16 +1,7 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/tls"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"math/big"
-	"net"
 	"net/http"
-	"time"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/olivere/elastic/v7"
@@ -18,53 +9,6 @@ import (
 
 	"gitlab.com/peerdb/search"
 )
-
-func genereateCertificate() tls.Certificate {
-	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		panic(err)
-	}
-
-	keyUsage := x509.KeyUsageDigitalSignature
-	notBefore := time.Now()
-	notAfter := notBefore.Add(365 * 24 * time.Hour)
-
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		panic(err)
-	}
-
-	template := x509.Certificate{
-		SerialNumber: serialNumber,
-		Subject: pkix.Name{
-			Organization: []string{"PeerDB Search"},
-		},
-		NotBefore:             notBefore,
-		NotAfter:              notAfter,
-		KeyUsage:              keyUsage,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
-		IPAddresses:           []net.IP{net.ParseIP("127.0.0.1")},
-		DNSNames:              []string{"localhost"},
-	}
-
-	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
-	if err != nil {
-		panic(err)
-	}
-
-	cert, err := x509.ParseCertificate(derBytes)
-	if err != nil {
-		panic(err)
-	}
-
-	return tls.Certificate{
-		Certificate: [][]byte{cert.Raw},
-		PrivateKey:  priv,
-		Leaf:        cert,
-	}
-}
 
 func listen(config *Config) errors.E {
 	client, err := elastic.NewClient()
@@ -84,10 +28,7 @@ func listen(config *Config) errors.E {
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
-		TLSConfig: &tls.Config{
-			Certificates: []tls.Certificate{genereateCertificate()},
-		},
 	}
 
-	return errors.WithStack(server.ListenAndServeTLS("", ""))
+	return errors.WithStack(server.ListenAndServeTLS(config.CertFile, config.KeyFile))
 }
