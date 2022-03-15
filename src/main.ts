76a4b5d196009eeb821758c046535e9cad5c5e96
Mitar <mitar.git@tnode.com>
2022-03-16 00:38:26 +0100

    Remove scss.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue"
 import { createRouter, createWebHistory } from "vue-router"
 import App from "@/App.vue"
 import { routes } from "@/../routes.json"
-import "./main.scss"
+import "./main.css"
 
 const router = createRouter({
   history: createWebHistory(),
