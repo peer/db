0a5037c6dc9292c8aadef0a93d2180d0d3fd599c
Mitar <mitar.git@tnode.com>
2022-03-17 09:32:01 +0100

    Distances and readonly.

--- src/views/HomeGet.vue
+++ src/views/HomeGet.vue
@@ -1,26 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue"
+
+const progress = ref(false)
+
+function onSubmit() {
+  progress.value = true
+}
+</script>
 
 <template>
-  <div class="flex flex-grow flex-col">
+  <form :readonly="progress" class="flex flex-grow flex-col" @submit.prevent="onSubmit">
     <div class="flex flex-grow basis-0 flex-col-reverse">
       <h1 class="mb-10 p-4 text-center text-5xl font-bold">Wikipedia Search</h1>
     </div>
     <div class="flex justify-center">
       <input
+        :readonly="progress"
         type="text"
         class="mx-4 w-full max-w-2xl rounded border-neutral-300 bg-white shadow-sm hover:border-neutral-400 focus:border-primary-500 sm:w-4/5 md:w-2/3 lg:w-1/2"
       />
     </div>
-    <div class="flex-grow basis-0 text-center">
+    <div class="flex-grow basis-0 pt-4 text-center">
       <button
-        class="mx-4 my-px mt-5 rounded bg-primary-600 px-6 py-2.5 font-medium uppercase leading-tight text-white shadow-sm outline-none hover:bg-primary-700 focus:ring-2 focus:ring-primary-500 focus:ring-offset-1 active:bg-primary-500"
+        :readonly="progress"
+        type="submit"
+        class="mx-4 my-px rounded bg-primary-600 px-6 py-2.5 font-medium uppercase leading-tight text-white shadow-sm outline-none hover:bg-primary-700 focus:ring-2 focus:ring-primary-500 focus:ring-offset-1 active:bg-primary-500"
       >
         Search
       </button>
     </div>
-  </div>
+  </form>
   <Teleport to="footer">
-    <div class="flex justify-between px-4 py-3">
+    <div class="flex justify-between px-4 py-4 leading-none">
       <ul class="flex gap-x-4">
         <!-- <li><router-link :to="{ name: 'HomeGet' }" class="link">About</router-link></li>
         <li><router-link :to="{ name: 'HomeGet' }" class="link">Help</router-link></li>
