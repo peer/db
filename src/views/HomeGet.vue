a675fa83d15e6d3b6eafdd00584a2ab4fe5c415f
Mitar <mitar.git@tnode.com>
2022-03-17 09:02:53 +0100

    Lighter ring.

--- src/views/HomeGet.vue
+++ src/views/HomeGet.vue
@@ -8,12 +8,12 @@
     <div class="flex justify-center">
       <input
         type="text"
-        class="mx-4 w-full max-w-2xl rounded border-neutral-300 bg-white shadow-sm hover:border-neutral-400 focus:border-primary-600 sm:w-4/5 md:w-2/3 lg:w-1/2"
+        class="mx-4 w-full max-w-2xl rounded border-neutral-300 bg-white shadow-sm hover:border-neutral-400 focus:border-primary-500 sm:w-4/5 md:w-2/3 lg:w-1/2"
       />
     </div>
     <div class="flex-grow basis-0 text-center">
       <button
-        class="mx-4 my-px mt-5 rounded bg-primary-600 px-6 py-2.5 font-medium uppercase leading-tight text-white shadow-sm outline-none hover:bg-primary-700 focus:ring-2 focus:ring-primary-600 focus:ring-offset-1 active:bg-primary-500"
+        class="mx-4 my-px mt-5 rounded bg-primary-600 px-6 py-2.5 font-medium uppercase leading-tight text-white shadow-sm outline-none hover:bg-primary-700 focus:ring-2 focus:ring-primary-500 focus:ring-offset-1 active:bg-primary-500"
       >
         Search
       </button>
