02c5c399dd156da88da970bbc262a0c57b3fd498
Mitar <mitar.git@tnode.com>
2022-03-19 00:50:56 +0100

    Progress bar.

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import ProgressBar from "@/components/ProgressBar.vue"
+
 defineProps({
   progress: {
     type: Number,
@@ -10,9 +12,10 @@ defineProps({
 <template>
   <button
     :readonly="progress > 0"
-    class="rounded bg-primary-600 px-6 py-2.5 font-medium uppercase leading-tight text-white shadow-sm outline-none hover:bg-primary-700 focus:ring-2 focus:ring-primary-500 focus:ring-offset-1 active:bg-primary-500"
+    class="relative rounded bg-primary-600 px-6 py-2.5 font-medium uppercase leading-tight text-white shadow-sm outline-none hover:bg-primary-700 focus:ring-2 focus:ring-primary-500 focus:ring-offset-1 active:bg-primary-500"
     :class="progress > 0 ? 'cursor-default bg-primary-300 text-gray-100 hover:bg-primary-300 focus:ring-primary-300 active:bg-primary-300' : ''"
   >
     <slot></slot>
+    <ProgressBar :progress="progress" class="absolute inset-x-0 bottom-0 rounded-b" />
   </button>
 </template>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+  progress: {
+    type: Number,
+    default: 0,
+  },
+})
+</script>
+
+<template>
+  <div v-if="progress > 0" class="h-1 w-full overflow-hidden">
+    <div :style="{ width: (1 / progress) * 100 + '%' }" class="relative h-full overflow-hidden">
+      <div class="progress-bar-long absolute inset-0 bg-secondary-300" style="right: 100%"></div>
+      <div class="progress-bar-short absolute inset-0 bg-secondary-300" style="right: 100%"></div>
+    </div>
+  </div>
+</template>
