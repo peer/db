9e66b719daf48f6fab0b036f2f8425be65adb79a
Mitar <mitar.git@tnode.com>
2022-03-20 22:35:15 +0100

    Optimize.

--- src/views/DocumentSearch.vue
+++ src/views/DocumentSearch.vue
@@ -64,7 +64,7 @@ watch(
 
 <template>
   <Teleport to="header">
-    <div class="fixed flex w-full flex-grow gap-x-1 border-b border-slate-400 bg-slate-300 p-1 shadow sm:gap-x-4 sm:p-4 sm:pl-0">
+    <div class="fixed flex w-full flex-grow gap-x-1 border-b border-slate-400 bg-slate-300 p-1 shadow will-change-transform sm:gap-x-4 sm:p-4 sm:pl-0">
       <router-link :to="{ name: 'HomeGet' }" class="group -my-4 hidden border-r border-slate-400 outline-none hover:bg-slate-400 active:bg-slate-200 sm:block">
         <GlobeIcon class="m-4 h-10 w-10 rounded group-focus:ring-2 group-focus:ring-primary-500" />
       </router-link>
