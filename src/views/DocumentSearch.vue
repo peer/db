ed4e143e68b878b1ec2be8e790899ab373e47923
Mitar <mitar.git@tnode.com>
2022-03-22 08:50:55 +0100

    Sticky navbar.
    But sometimes it jumps down on fast scrolling down. Progress bar is also not always at the top.

--- src/views/DocumentSearch.vue
+++ src/views/DocumentSearch.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch, readonly } from "vue"
+import { ref, watch, readonly, onMounted, onBeforeUnmount } from "vue"
 import { useRoute, useRouter } from "vue-router"
 import { GlobeIcon } from "@heroicons/vue/outline"
 import { SearchIcon } from "@heroicons/vue/solid"
@@ -13,6 +13,7 @@ const router = useRouter()
 const formProgress = ref(0)
 const dataProgress = ref(0)
 const form = ref()
+
 const _results = ref()
 const _total = ref(0)
 const _moreThanTotal = ref(false)
@@ -60,11 +61,59 @@ watch(
     immediate: true,
   },
 )
+
+const navbar = ref()
+let position = ref("absolute")
+let navbarTop = ref(0)
+let lastScrollPosition = 0
+const supportPageOffset = window.pageYOffset !== undefined
+
+function onScroll() {
+  const currentScrollPosition = supportPageOffset ? window.pageYOffset : document.documentElement.scrollTop
+  if (currentScrollPosition <= 0) {
+    position.value = "absolute"
+    navbarTop.value = 0
+    lastScrollPosition = 0
+    return
+  }
+
+  if (currentScrollPosition > lastScrollPosition) {
+    if (position.value !== "absolute") {
+      let { top } = navbar.value.getBoundingClientRect()
+      position.value = "absolute"
+      navbarTop.value = lastScrollPosition + top
+    }
+  } else if (currentScrollPosition < lastScrollPosition) {
+    if (position.value !== "fixed") {
+      const { top, height } = navbar.value.getBoundingClientRect()
+      if (top >= 0) {
+        navbarTop.value = 0
+        position.value = "fixed"
+      } else if (top < -height) {
+        navbarTop.value = lastScrollPosition - height
+      }
+    }
+  }
+
+  lastScrollPosition = currentScrollPosition
+}
+
+onMounted(() => {
+  window.addEventListener("scroll", onScroll, { passive: true })
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", onScroll)
+})
 </script>
 
 <template>
   <Teleport to="header">
-    <div class="fixed flex w-full flex-grow gap-x-1 border-b border-slate-400 bg-slate-300 p-1 shadow will-change-transform sm:gap-x-4 sm:p-4 sm:pl-0">
+    <div
+      ref="navbar"
+      class="flex w-full flex-grow gap-x-1 border-b border-slate-400 bg-slate-300 p-1 shadow will-change-transform sm:gap-x-4 sm:p-4 sm:pl-0"
+      :style="{ position: position, top: navbarTop + 'px' }"
+    >
       <router-link :to="{ name: 'HomeGet' }" class="group -my-4 hidden border-r border-slate-400 outline-none hover:bg-slate-400 active:bg-slate-200 sm:block">
         <GlobeIcon class="m-4 h-10 w-10 rounded group-focus:ring-2 group-focus:ring-primary-500" />
       </router-link>
