52828a2a2deb1861a3f50f2dea24f982ef077629
Mitar <mitar.git@tnode.com>
2022-03-27 15:02:08 +0200

    Extract navbar.

---
+++ src/components/NavBarSearch.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { useRouter, useRoute } from "vue-router"
+import { SearchIcon } from "@heroicons/vue/solid"
+import InputText from "@/components/InputText.vue"
+import Button from "@/components/Button.vue"
+import { postSearch } from "@/search"
+
+const route = useRoute()
+
+const router = useRouter()
+const form = ref()
+const progress = ref(0)
+
+async function onSubmit() {
+  await postSearch(router, form.value, progress)
+}
+</script>
+
+<template>
+  <form ref="form" :disabled="progress > 0" class="flex flex-grow gap-x-1 sm:gap-x-4" @submit.prevent="onSubmit">
+    <input type="hidden" name="s" :value="route.query.s" />
+    <InputText :progress="progress" name="q" class="max-w-xl flex-grow" :value="route.query.s ? route.query.q : ''" />
+    <Button :progress="progress" type="submit" class="px-3.5">
+      <SearchIcon class="h-5 w-5 sm:hidden" />
+      <span class="hidden sm:inline">Search</span>
+    </Button>
+  </form>
+</template>
--- src/views/DocumentSearch.vue
+++ src/views/DocumentSearch.vue
@@ -1,39 +1,19 @@
 <script setup lang="ts">
 import { ref } from "vue"
-import { useRouter, useRoute } from "vue-router"
-import { SearchIcon } from "@heroicons/vue/solid"
-import InputText from "@/components/InputText.vue"
-import Button from "@/components/Button.vue"
 import SearchResult from "@/components/SearchResult.vue"
 import NavBar from "@/components/NavBar.vue"
 import Footer from "@/components/Footer.vue"
-import { postSearch, useSearch } from "@/search"
-
-const route = useRoute()
+import NavBarSearch from "@/components/NavBarSearch.vue"
+import { useSearch } from "@/search"
 
 const dataProgress = ref(0)
 const { docs, total, moreThanTotal, hasMore, loadMore } = useSearch(dataProgress)
-
-const router = useRouter()
-const form = ref()
-const formProgress = ref(0)
-
-async function onSubmit() {
-  await postSearch(router, form.value, formProgress)
-}
 </script>
 
 <template>
   <Teleport to="header">
     <NavBar :progress="dataProgress">
-      <form ref="form" :disabled="formProgress > 0" class="flex flex-grow gap-x-1 sm:gap-x-4" @submit.prevent="onSubmit">
-        <input type="hidden" name="s" :value="route.query.s" />
-        <InputText :progress="formProgress" name="q" class="max-w-xl flex-grow" :value="route.query.q" />
-        <Button :progress="formProgress" type="submit" class="px-3.5">
-          <SearchIcon class="h-5 w-5 sm:hidden" />
-          <span class="hidden sm:inline">Search</span>
-        </Button>
-      </form>
+      <NavBarSearch />
     </NavBar>
   </Teleport>
   <div class="mt-12 flex w-full flex-col gap-y-1 border-t border-transparent p-1 sm:mt-[4.5rem] sm:gap-y-4 sm:p-4">
