a5d2c79baea5db7dec324c905ed146644dbc44f6
Mitar <mitar.git@tnode.com>
2022-07-02 01:01:38 +0200

    Reload certificates daily.

--- cmd/search/search.go
+++ cmd/search/search.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net/http"
 
@@ -39,15 +40,45 @@ func listen(config *Config) errors.E {
 		return err
 	}
 
+	manager := CertificateManager{
+		CertFile: config.CertFile,
+		KeyFile:  config.KeyFile,
+		Log:      config.Log,
+	}
+
+	err = manager.Start()
+	if err != nil {
+		return err
+	}
+	defer manager.Stop()
+
 	// TODO: Implement graceful shutdown.
 	server := &http.Server{
 		Addr:        listenAddr,
 		Handler:     handler,
 		ErrorLog:    log.New(config.Log, "", 0),
 		ConnContext: s.ConnContext,
+		TLSConfig: &tls.Config{
+			MinVersion:       tls.VersionTLS12,
+			CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			CipherSuites: []uint16{
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+				tls.TLS_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			},
+			GetCertificate: manager.GetCertificate,
+		},
 	}
 
 	config.Log.Info().Msgf("starting on %s", listenAddr)
 
-	return errors.WithStack(server.ListenAndServeTLS(config.CertFile, config.KeyFile))
+	return errors.WithStack(server.ListenAndServeTLS("", ""))
 }
