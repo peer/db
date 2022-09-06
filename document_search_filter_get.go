5eed03a726dda4f7fddf6b2971ca192c0f6b1232
Mitar <mitar.git@tnode.com>
2022-09-06 14:12:07 +0200

    Provide values for filters.

--- document_search_filter_get.go
+++ document_search_filter_get.go
@@ -14,21 +14,11 @@ import (
 	"gitlab.com/peerdb/search/identifier"
 )
 
-type propsAggregations struct {
-	Props struct {
-		Buckets []struct {
-			Key  string `json:"key"`
-			Docs struct {
-				Count int64 `json:"doc_count"`
-			} `json:"docs"`
-		} `json:"buckets"`
-	} `json:"props"`
-	Total struct {
-		Value int64 `json:"value"`
-	} `json:"total"`
+type filteredPropsAggregations struct {
+	Filter propsAggregations `json:"filter"`
 }
 
-func (s *Service) DocumentSearchFiltersGetJSON(w http.ResponseWriter, req *http.Request, params Params) {
+func (s *Service) DocumentSearchFilterGetGetJSON(w http.ResponseWriter, req *http.Request, params Params) {
 	contentEncoding := gddo.NegotiateContentEncoding(req, allCompressions)
 	if contentEncoding == "" {
 		s.NotAcceptable(w, req, nil)
@@ -40,6 +30,12 @@ func (s *Service) DocumentSearchFiltersGetJSON(w http.ResponseWriter, req *http.
 
 	id := params["s"]
 	if !identifier.Valid(id) {
+		s.BadRequest(w, req, nil)
+		return
+	}
+
+	prop := params["prop"]
+	if !identifier.Valid(prop) {
 		s.BadRequest(w, req, nil)
 		return
 	}
@@ -56,16 +52,19 @@ func (s *Service) DocumentSearchFiltersGetJSON(w http.ResponseWriter, req *http.
 
 	query := s.getSearchQuery(sh)
 	aggregation := elastic.NewNestedAggregation().Path("active.rel").SubAggregation(
-		"props",
-		elastic.NewTermsAggregation().Field("active.rel.prop._id").Size(maxResultsCount).OrderByAggregation("docs", false).SubAggregation(
-			"docs",
-			elastic.NewReverseNestedAggregation(),
+		"filter",
+		elastic.NewFilterAggregation().Filter(elastic.NewTermQuery("active.rel.prop._id", prop)).SubAggregation(
+			"props",
+			elastic.NewTermsAggregation().Field("active.rel.to._id").Size(maxResultsCount).OrderByAggregation("docs", false).SubAggregation(
+				"docs",
+				elastic.NewReverseNestedAggregation(),
+			),
+		).SubAggregation(
+			"total",
+			// Cardinality aggregation returns the count of all buckets. It can be at most s.propertiesTotalInt,
+			// so we set precision threshold to twice as much to try to always get precise counts.
+			elastic.NewCardinalityAggregation().Field("active.rel.to._id").PrecisionThreshold(2*int64(s.propertiesTotalInt)),
 		),
-	).SubAggregation(
-		"total",
-		// Cardinality aggregation returns the count of all buckets. It can be at most s.propertiesTotalInt,
-		// so we set precision threshold to twice as much to try to always get precise counts.
-		elastic.NewCardinalityAggregation().Field("active.rel.prop._id").PrecisionThreshold(2*int64(s.propertiesTotalInt)),
 	)
 	searchService := s.getSearchService(req).Size(0).Query(query).Aggregation("rel", aggregation)
 
@@ -79,7 +78,7 @@ func (s *Service) DocumentSearchFiltersGetJSON(w http.ResponseWriter, req *http.
 	timing.NewMetric("esi").Duration = time.Duration(res.TookInMillis) * time.Millisecond
 
 	m = timing.NewMetric("d").Start()
-	var props propsAggregations
+	var props filteredPropsAggregations
 	err = json.Unmarshal(res.Aggregations["rel"], &props)
 	m.Stop()
 	if err != nil {
@@ -87,17 +86,17 @@ func (s *Service) DocumentSearchFiltersGetJSON(w http.ResponseWriter, req *http.
 		return
 	}
 
-	results := make([]searchResult, len(props.Props.Buckets))
-	for i, bucket := range props.Props.Buckets {
+	results := make([]searchResult, len(props.Filter.Props.Buckets))
+	for i, bucket := range props.Filter.Props.Buckets {
 		results[i] = searchResult{ID: bucket.Key, Count: bucket.Docs.Count}
 	}
 
 	// Cardinality count is approximate, so we make sure the total is sane.
 	// See: https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-metrics-cardinality-aggregation.html#_counts_are_approximate
-	if int64(len(props.Props.Buckets)) > props.Total.Value {
-		props.Total.Value = int64(len(props.Props.Buckets))
+	if int64(len(props.Filter.Props.Buckets)) > props.Filter.Total.Value {
+		props.Filter.Total.Value = int64(len(props.Filter.Props.Buckets))
 	}
-	total := strconv.FormatInt(props.Total.Value, 10) //nolint:gomnd
+	total := strconv.FormatInt(props.Filter.Total.Value, 10) //nolint:gomnd
 
 	metadata := http.Header{
 		"Total": {total},
