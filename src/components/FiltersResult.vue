b21b764e620af8224e0526236909a7aca98f8c4f
Mitar <mitar.git@tnode.com>
2022-09-12 21:43:40 +0200

    Better position of none.

--- src/components/FiltersResult.vue
+++ src/components/FiltersResult.vue
@@ -14,6 +14,16 @@ const progress = ref(0)
 const { docs, total } = useFilterValues(props.property, progress)
 
 const hasLoaded = computed(() => props.property?.name?.en)
+const docsWithNone = computed(() => {
+  if (!docs.value.length) {
+    return docs.value
+  } else if (props.property._count >= props.searchTotal) {
+    return docs.value
+  }
+  const res = [...docs.value, { _count: props.searchTotal - props.property._count }]
+  res.sort((a, b) => b._count - a._count)
+  return res
+})
 </script>
 
 <template>
@@ -25,17 +35,17 @@ const hasLoaded = computed(() => props.property?.name?.en)
       </div>
       <div>({{ total }})</div>
       <ul>
-        <li v-for="doc in docs" :key="doc._id">
+        <li v-for="doc in docsWithNone" :key="doc._id">
           <template v-if="doc.name?.en">
             <RouterLink :to="{ name: 'DocumentGet', params: { id: doc._id } }" class="link">{{ doc.name.en }}</RouterLink> ({{ doc._count }})
           </template>
+          <template v-else-if="!doc._id"><i>none</i> ({{ doc._count }})</template>
           <div v-else class="flex animate-pulse">
             <div class="flex-1 space-y-4">
               <div class="my-2 h-2 w-52 rounded bg-slate-200"></div>
             </div>
           </div>
         </li>
-        <li v-if="property._count < searchTotal"><i>none</i> ({{ searchTotal - property._count }})</li>
       </ul>
     </div>
     <div v-else class="flex animate-pulse">
