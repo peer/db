2685e9d2d9e56ce234213f695fd68f8240f39571
Mitar <mitar.git@tnode.com>
2022-09-13 13:00:27 +0200

    Upgrade Tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 
 const colors = require("tailwindcss/colors")
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
