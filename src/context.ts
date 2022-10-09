b16da9bc02036a3131d56734bdee641bf6d104eb
Mitar <mitar.git@tnode.com>
2022-10-09 22:39:10 +0200

    More parameters to fetch.

--- src/context.ts
+++ src/context.ts
@@ -2,4 +2,14 @@ import type { SiteContext } from "@/types"
 
 // TODO: Use import with import assertion?
 //       See: https://github.com/vitejs/vite/issues/4934
-export default (await fetch("/context.json").then((response) => response.json())) as SiteContext
+export default (await fetch("/context.json", {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
+  mode: "same-origin",
+  credentials: "omit",
+  redirect: "error",
+  referrer: document.location.href,
+  referrerPolicy: "strict-origin-when-cross-origin",
+}).then((response) => response.json())) as SiteContext
