90d363539bb85b32efa9cc8f9ad0d9947be58d30
Mitar <mitar.git@tnode.com>
2022-11-02 10:36:40 +0100

    Fix preload in Firefox.

--- src/context.ts
+++ src/context.ts
@@ -2,11 +2,12 @@ import type { SiteContext } from "@/types"
 
 // TODO: Use import with import assertion?
 //       See: https://github.com/vitejs/vite/issues/4934
+// redirect: "error" cannot be set because then preload is not matched in Firefox.
 export default (await fetch("/context.json", {
   method: "GET",
+  // Mode and credentials match crossorigin=anonymous in link preload header.
   mode: "cors",
   credentials: "same-origin",
-  redirect: "error",
   referrer: document.location.href,
   referrerPolicy: "strict-origin-when-cross-origin",
 }).then((response) => response.json())) as SiteContext
