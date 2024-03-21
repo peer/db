4f2deccb87f4c4e78a6b32528094e7e6b9b5e59d
Mitar <mitar.git@tnode.com>
2024-03-21 12:04:21 +0100

    Initialize index for testing.

---
+++ populate_test.go
@@ -0,0 +1,37 @@
+package peerdb_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/rs/zerolog"
+	"gitlab.com/peerdb/peerdb"
+	z "gitlab.com/tozd/go/zerolog"
+)
+
+func TestMain(m *testing.M) {
+	elastic := os.Getenv("ELASTIC")
+	if elastic == "" {
+		elastic = peerdb.DefaultElastic
+	}
+
+	globals := &peerdb.Globals{
+		LoggingConfig: z.LoggingConfig{
+			Logger: zerolog.Nop(),
+		},
+		Elastic:   elastic,
+		Index:     peerdb.DefaultIndex,
+		SizeField: false,
+	}
+
+	populate := peerdb.PopulateCommand{}
+
+	errE := populate.Run(globals)
+	if errE != nil {
+		fmt.Fprintf(os.Stderr, "% -+#.1v\n", errE)
+		os.Exit(1)
+	}
+
+	m.Run()
+}
