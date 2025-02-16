123b0e2fe5fadcac03fce8879cdc8e308b1beb70
Mitar <mitar.git@tnode.com>
2025-02-16 10:20:12 +0100

    Log size of cached file.

--- cmd/products/utils.go
+++ cmd/products/utils.go
@@ -204,9 +204,6 @@ func cachedDownload(ctx context.Context, httpClient *retryablehttp.Client, logge
 	if err != nil {
 		if errors.Is(err, fs.ErrExist) {
 			// The simple code path: the cached file already exist.
-			logger.Info().Str("url", url).
-				Msg("using cached file")
-
 			// We open it for reading and return it. We do not attempt to resume downloading
 			// because we assume that the file has already been fully downloaded. We are using
 			// x.NewRetryableResponse to make sure that downloading is transparently retried and we
@@ -226,6 +223,10 @@ func cachedDownload(ctx context.Context, httpClient *retryablehttp.Client, logge
 			if err != nil {
 				return nil, 0, errors.WithStack(err)
 			}
+
+			logger.Info().Str("url", url).
+				Int64("total", cachedSize).
+				Msg("using cached file")
 
 			return cachedReadFile, cachedSize, nil
 		}
