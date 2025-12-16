f874eade91ab75d36ba3d2359f6b3b0266e3765e
Mitar <mitar.git@tnode.com>
2025-12-16 13:55:52 +0100

    Use new frontend API.

--- src/partials/SearchResultsHeader.vue
+++ src/partials/SearchResultsHeader.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { DeepReadonly } from "vue"
 
-import type { ClientSearchState, SearchViewType, SelectButtonOption } from "@/types"
+import type { ClientSearchSession, SearchViewType, SelectButtonOption } from "@/types"
 
 import { computed } from "vue"
 import { Bars4Icon, TableCellsIcon } from "@heroicons/vue/20/solid"
@@ -9,7 +9,7 @@ import { Bars4Icon, TableCellsIcon } from "@heroicons/vue/20/solid"
 import SelectButton from "@/components/SelectButton.vue"
 
 const props = defineProps<{
-  searchState: DeepReadonly<ClientSearchState | null>
+  searchSession: DeepReadonly<ClientSearchSession | null>
   searchTotal: number | null
   searchMoreThanTotal: boolean
   searchView: SearchViewType
@@ -50,28 +50,28 @@ const selectButtonOptions: SelectButtonOption<SearchViewType>[] = [
 // TODO: Use a computed property instead of computing countFilters multiple times.
 
 function countFilters(): number {
-  if (!props.searchState) {
+  if (!props.searchSession) {
     return 0
   }
-  if (!props.searchState.filters) {
+  if (!props.searchSession.filters) {
     return 0
   }
 
   let n = 0
-  for (const values of Object.values(props.searchState.filters.rel)) {
+  for (const values of Object.values(props.searchSession.filters.rel)) {
     n += values.length
   }
-  for (const value of Object.values(props.searchState.filters.amount)) {
+  for (const value of Object.values(props.searchSession.filters.amount)) {
     if (value) {
       n++
     }
   }
-  for (const value of Object.values(props.searchState.filters.time)) {
+  for (const value of Object.values(props.searchSession.filters.time)) {
     if (value) {
       n++
     }
   }
-  for (const values of Object.values(props.searchState.filters.str)) {
+  for (const values of Object.values(props.searchSession.filters.str)) {
     n += values.length
   }
   return n
@@ -81,12 +81,12 @@ function countFilters(): number {
 <template>
   <div class="flex flex-row gap-x-1 sm:gap-x-4">
     <div class="bg-slate-200 px-2 sm:px-4 py-1 sm:py-2 rounded flex flex-row justify-between items-center w-full gap-x-1 sm:gap-x-4">
-      <div v-if="searchState === null">Loading...</div>
-      <div v-else-if="searchState.q && countFilters() === 1">
-        Searching query <i>{{ searchState.q }}</i> and 1 active filter<template v-if="searchTotal === null">...</template><template v-else>.</template>
+      <div v-if="searchSession === null">Loading...</div>
+      <div v-else-if="searchSession.query && countFilters() === 1">
+        Searching query <i>{{ searchSession.query }}</i> and 1 active filter<template v-if="searchTotal === null">...</template><template v-else>.</template>
       </div>
-      <div v-else-if="searchState.q">
-        Searching query <i>{{ searchState.q }}</i> and {{ countFilters() }} active filters<template v-if="searchTotal === null">...</template
+      <div v-else-if="searchSession.query">
+        Searching query <i>{{ searchSession.query }}</i> and {{ countFilters() }} active filters<template v-if="searchTotal === null">...</template
         ><template v-else>.</template>
       </div>
       <div v-else-if="countFilters() === 1">
