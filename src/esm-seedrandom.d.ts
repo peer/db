3f199be24e8d40b1fc63aa27f4de256d95a8f5bd
Mitar <mitar.git@tnode.com>
2025-12-26 01:00:26 +0100

    Lint.

--- src/esm-seedrandom.d.ts
+++ src/esm-seedrandom.d.ts
@@ -1,4 +1,13 @@
 // esm-seedrandom package is missing types.
 // See: https://github.com/shanewholloway/js-esm-seedrandom/issues/1
 
-declare module "esm-seedrandom"
+declare module "esm-seedrandom" {
+  export interface PRNG {
+    (): number
+    quick(): number
+    int32(): number
+    double(): number
+  }
+
+  export function prng_alea(seed?: string): PRNG
+}
