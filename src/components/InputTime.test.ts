4a4e381d6f7b72745dcb6890d10005f626e66499
Matic Lang <matic.lang>
2026-01-07 22:04:41 +0100

    Add tests for validation function.

--- src/components/InputTime.test.ts
+++ src/components/InputTime.test.ts
@@ -1,6 +1,6 @@
 import { assert, describe, test } from "vitest"
 
-import { normalizeForParsing } from "./InputTime.vue"
+import { normalizeForParsing, progressiveValidate } from "./InputTime.vue"
 
 // TODO: Enable once eslint parser for extra files is used.
 //       See: https://github.com/ota-meshi/typescript-eslint-parser-for-extra-files/issues/162
@@ -76,5 +76,106 @@ describe("normalizeForParsing", () => {
     assert.equal(normalizedForParsingExposed("2022-01-01"), "2022-01-01")
     assert.equal(normalizedForParsingExposed("2022-01-01 12:34"), "2022-01-01 12:34")
     assert.equal(normalizedForParsingExposed("-2022-01-01 12:34:21"), "-2022-01-01 12:34:21")
+  })
+})
+
+// TODO: Enable once eslint parser for extra files is used.
+//       See: https://github.com/ota-meshi/typescript-eslint-parser-for-extra-files/issues/162
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+const progressiveValidateExposed = (value: string) => progressiveValidate(value)
+
+describe("progressiveValidate", () => {
+  test("returns empty string for empty input", () => {
+    assert.equal(progressiveValidateExposed(""), "")
+
+    assert.notEqual(progressiveValidateExposed(""), "Invalid timestamp structure.")
+  })
+
+  test("allows year in progress", () => {
+    assert.equal(progressiveValidateExposed("2"), "")
+    assert.equal(progressiveValidateExposed("20"), "")
+    assert.equal(progressiveValidateExposed("202"), "")
+    assert.equal(progressiveValidateExposed("2023"), "")
+
+    assert.notEqual(progressiveValidateExposed("2023"), "Months need to be between 0-12.")
+  })
+
+  test("validates month in progress", () => {
+    assert.equal(progressiveValidateExposed("2023-"), "")
+    assert.equal(progressiveValidateExposed("2023-0"), "")
+    assert.equal(progressiveValidateExposed("2023-1"), "")
+    assert.equal(progressiveValidateExposed("2023-12"), "")
+    assert.equal(progressiveValidateExposed("2023-13"), "Months need to be between 0-12.")
+
+    assert.notEqual(progressiveValidateExposed("2023-12"), "Invalid timestamp structure.")
+    assert.notEqual(progressiveValidateExposed("2023-13"), "")
+  })
+
+  test("validates day in progress", () => {
+    assert.equal(progressiveValidateExposed("2023-1-"), "")
+    assert.equal(progressiveValidateExposed("2023-1-0"), "")
+    assert.equal(progressiveValidateExposed("2023-1-1"), "")
+    assert.equal(progressiveValidateExposed("2023-1-1 1"), "")
+    assert.equal(progressiveValidateExposed("2023-0-1"), "Months cannot be 0 when days are not 0.")
+    assert.equal(progressiveValidateExposed("2023-00-01"), "Months cannot be 0 when days are not 0.")
+    assert.equal(progressiveValidateExposed("2023-13-1"), "Months need to be between 0-12.")
+    assert.equal(progressiveValidateExposed("2023-2-30"), "Day must be between 0-28.")
+    assert.equal(progressiveValidateExposed("2015-2-30"), "Day must be between 0-28.")
+
+    assert.notEqual(progressiveValidateExposed("2023-1-1"), "Invalid timestamp structure.")
+    assert.notEqual(progressiveValidateExposed("2023-0-1"), "")
+    assert.notEqual(progressiveValidateExposed("2023-13-1"), "")
+    assert.notEqual(progressiveValidateExposed("2023-2-30"), "")
+  })
+
+  test("validates hours", () => {
+    assert.equal(progressiveValidateExposed("2023-12-31 0"), "")
+    assert.equal(progressiveValidateExposed("2023-12-31 23"), "")
+    assert.equal(progressiveValidateExposed("2023-12-31 24"), "Hours needs to be between 0-23.")
+    assert.equal(progressiveValidateExposed("2023-13-31 12"), "Months need to be between 1-12.")
+    assert.equal(progressiveValidateExposed("2023-2-30 12"), "Day must be between 1-28.")
+
+    assert.notEqual(progressiveValidateExposed("2023-12-31 23"), "Invalid timestamp structure.")
+    assert.notEqual(progressiveValidateExposed("2023-12-31 24"), "")
+    assert.notEqual(progressiveValidateExposed("2023-13-31 12"), "")
+    assert.notEqual(progressiveValidateExposed("2023-2-30 12"), "")
+  })
+
+  test("do not allows minutes in progress", () => {
+    assert.equal(progressiveValidateExposed("2023-12-31 12:"), "Invalid timestamp structure.")
+  })
+
+  test("validates minutes", () => {
+    assert.equal(progressiveValidateExposed("2023-12-31 12:0"), "")
+    assert.equal(progressiveValidateExposed("2023-12-31 12:59"), "")
+    assert.equal(progressiveValidateExposed("2023-12-31 12:60"), "Minutes need to be between 0-59.")
+    assert.equal(progressiveValidateExposed("2023-12-31 24:00"), "Hours needs to be between 0-23.")
+
+    assert.notEqual(progressiveValidateExposed("2023-12-31 12:59"), "Invalid timestamp structure.")
+    assert.notEqual(progressiveValidateExposed("2023-12-31 12:60"), "")
+    assert.notEqual(progressiveValidateExposed("2023-12-31 24:00"), "")
+  })
+
+  test("do not allows seconds in progress", () => {
+    assert.equal(progressiveValidateExposed("2023-12-31 12:34:"), "Invalid timestamp structure.")
+  })
+
+  test("validates seconds", () => {
+    assert.equal(progressiveValidateExposed("2023-12-31 12:34:0"), "")
+    assert.equal(progressiveValidateExposed("2023-12-31 12:34:59"), "")
+    assert.equal(progressiveValidateExposed("2023-12-31 12:34:60"), "Seconds need to be between 0-59.")
+    assert.equal(progressiveValidateExposed("2023-12-31 12:60:00"), "Minutes need to be between 0-59.")
+
+    assert.notEqual(progressiveValidateExposed("2023-12-31 12:34:59"), "Invalid timestamp structure.")
+    assert.notEqual(progressiveValidateExposed("2023-12-31 12:34:60"), "")
+    assert.notEqual(progressiveValidateExposed("2023-12-31 12:60:00"), "")
+  })
+
+  test("rejects invalid timestamp structure", () => {
+    assert.equal(progressiveValidateExposed("foo"), "Invalid timestamp structure.")
+    assert.equal(progressiveValidateExposed("2023--12"), "Invalid timestamp structure.")
+
+    assert.notEqual(progressiveValidateExposed("foo"), "")
+    assert.notEqual(progressiveValidateExposed("2023--12"), "")
   })
 })
