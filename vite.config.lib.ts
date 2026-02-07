a0e1d789174aab89778fc45496a74a96fcceddb8
Mitar <mitar.git@tnode.com>
2026-02-07 02:55:18 +0100

    Copy types over.

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -46,6 +46,10 @@ export default defineConfig({
           src: "src/theme.css",
           dest: ".",
         },
+        {
+          src: "src/**/*.d.ts",
+          dest: ".",
+        },
       ],
     }),
   ],
