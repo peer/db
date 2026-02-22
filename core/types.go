c09b443633e70d3392e6a9d1858ec06c85009cc7
Mitar <mitar.git@tnode.com>
2026-02-22 11:44:21 +0100

    Add JSON struct tags.

--- core/types.go
+++ core/types.go
@@ -8,7 +8,7 @@ import (
 
 // Ref represents a reference to another document by ID.
 type Ref struct {
-	ID []string
+	ID []string `json:"id"`
 }
 
 // Identifier is a string identifier.
@@ -31,60 +31,60 @@ type Unknown bool
 
 // Time represents a time with precision.
 type Time struct {
-	Timestamp time.Time
-	Precision document.TimePrecision
+	Timestamp time.Time              `json:"timestamp"`
+	Precision document.TimePrecision `json:"precision"`
 }
 
 // Interval represents a time interval.
 type Interval struct {
-	From          *Time
-	FromIsUnknown bool
-	To            *Time
-	ToIsUnknown   bool
+	From          *Time `json:"from,omitempty"`
+	FromIsUnknown bool  `json:"fromIsUnknown,omitempty"`
+	To            *Time `json:"to,omitempty"`
+	ToIsUnknown   bool  `json:"toIsUnknown,omitempty"`
 }
 
 // DocumentFields contains common fields for all documents.
 type DocumentFields struct {
-	ID         []string `                  documentid:""`
-	InstanceOf []Ref    `cardinality:"0.."               property:"INSTANCE_OF"`
+	ID         []string `                  documentid:"" json:"id"`
+	InstanceOf []Ref    `cardinality:"0.."               json:"instanceOf,omitempty" property:"INSTANCE_OF"`
 }
 
 // PropertyName represents a property name with language information.
 type PropertyName struct {
-	Name string `value:""`
+	Name string `json:"name" value:""`
 
-	InLanguage []Ref `cardinality:"0.." property:"IN_LANGUAGE"`
+	InLanguage []Ref `cardinality:"0.." json:"inLanguage,omitempty" property:"IN_LANGUAGE"`
 }
 
 // PropertyShortName represents a property short name with language information.
 type PropertyShortName struct {
-	ShortName string `value:""`
+	ShortName string `json:"shortName" value:""`
 
-	InLanguage []Ref `cardinality:"0.." property:"IN_LANGUAGE"`
+	InLanguage []Ref `cardinality:"0.." json:"inLanguage,omitempty" property:"IN_LANGUAGE"`
 }
 
 // PropertyDescription represents a property description with language information.
 type PropertyDescription struct {
-	Description HTML `value:""`
+	Description HTML `json:"description" value:""`
 
-	InLanguage []Ref `cardinality:"0.." property:"IN_LANGUAGE"`
+	InLanguage []Ref `cardinality:"0.." json:"inLanguage,omitempty" property:"IN_LANGUAGE"`
 }
 
 // PropertyInstruction represents a property instruction with language information.
 type PropertyInstruction struct {
-	Instruction HTML `value:""`
+	Instruction HTML `json:"instruction" value:""`
 
-	InLanguage []Ref `cardinality:"0.." property:"IN_LANGUAGE"`
+	InLanguage []Ref `cardinality:"0.." json:"inLanguage,omitempty" property:"IN_LANGUAGE"`
 }
 
 // PropertyFields contains fields specific to properties.
 type PropertyFields struct {
-	Name          []PropertyName        `cardinality:"1.."  property:"NAME"`
-	ShortName     []PropertyShortName   `cardinality:"0.."  property:"SHORT_NAME"`
-	Mnemonic      string                `cardinality:"0..1" property:"MNEMONIC"`
-	Description   []PropertyDescription `cardinality:"0.."  property:"DESCRIPTION"`
-	Instruction   []PropertyInstruction `cardinality:"0.."  property:"INSTRUCTION"`
-	SubpropertyOf []Ref                 `cardinality:"0.."  property:"SUBPROPERTY_OF"`
+	Name          []PropertyName        `cardinality:"1.."  json:"name"                    property:"NAME"`
+	ShortName     []PropertyShortName   `cardinality:"0.."  json:"shortName,omitempty"     property:"SHORT_NAME"`
+	Mnemonic      string                `cardinality:"0..1" json:"mnemonic,omitempty"      property:"MNEMONIC"`
+	Description   []PropertyDescription `cardinality:"0.."  json:"description,omitempty"   property:"DESCRIPTION"`
+	Instruction   []PropertyInstruction `cardinality:"0.."  json:"instruction,omitempty"   property:"INSTRUCTION"`
+	SubpropertyOf []Ref                 `cardinality:"0.."  json:"subpropertyOf,omitempty" property:"SUBPROPERTY_OF"`
 }
 
 // Property represents a property document.
@@ -95,32 +95,32 @@ type Property struct {
 
 // ClassName represents a class name with language information.
 type ClassName struct {
-	Name string `value:""`
+	Name string `json:"name" value:""`
 
-	InLanguage []Ref `cardinality:"0.." property:"IN_LANGUAGE"`
+	InLanguage []Ref `cardinality:"0.." json:"inLanguage,omitempty" property:"IN_LANGUAGE"`
 }
 
 // ClassShortName represents a class short name with language information.
 type ClassShortName struct {
-	ShortName string `value:""`
+	ShortName string `json:"shortName" value:""`
 
-	InLanguage []Ref `cardinality:"0.." property:"IN_LANGUAGE"`
+	InLanguage []Ref `cardinality:"0.." json:"inLanguage,omitempty" property:"IN_LANGUAGE"`
 }
 
 // ClassDescription represents a class description with language information.
 type ClassDescription struct {
-	Description HTML `value:""`
+	Description HTML `json:"description" value:""`
 
-	InLanguage []Ref `cardinality:"0.." property:"IN_LANGUAGE"`
+	InLanguage []Ref `cardinality:"0.." json:"inLanguage,omitempty" property:"IN_LANGUAGE"`
 }
 
 // ClassFields contains fields specific to classes.
 type ClassFields struct {
-	Name        []ClassName        `cardinality:"1.."  property:"NAME"`
-	ShortName   []ClassShortName   `cardinality:"0.."  property:"SHORT_NAME"`
-	Mnemonic    string             `cardinality:"0..1" property:"MNEMONIC"`
-	Description []ClassDescription `cardinality:"0.."  property:"DESCRIPTION"`
-	SubclassOf  []Ref              `cardinality:"0.."  property:"SUBCLASS_OF"`
+	Name        []ClassName        `cardinality:"1.."  json:"name"                  property:"NAME"`
+	ShortName   []ClassShortName   `cardinality:"0.."  json:"shortName,omitempty"   property:"SHORT_NAME"`
+	Mnemonic    string             `cardinality:"0..1" json:"mnemonic,omitempty"    property:"MNEMONIC"`
+	Description []ClassDescription `cardinality:"0.."  json:"description,omitempty" property:"DESCRIPTION"`
+	SubclassOf  []Ref              `cardinality:"0.."  json:"subclassOf,omitempty"  property:"SUBCLASS_OF"`
 }
 
 // Class represents a class document.
@@ -131,15 +131,15 @@ type Class struct {
 
 // VocabularyName represents a vocabulary name with language information.
 type VocabularyName struct {
-	Name string `value:""`
+	Name string `json:"name" value:""`
 
-	InLanguage []Ref `cardinality:"0.." property:"IN_LANGUAGE"`
+	InLanguage []Ref `cardinality:"0.." json:"inLanguage,omitempty" property:"IN_LANGUAGE"`
 }
 
 // VocabularyFields contains fields specific to vocabularies.
 type VocabularyFields struct {
-	Name []VocabularyName `cardinality:"1.." property:"NAME"`
-	Code []Identifier     `cardinality:"0.." property:"CODE"`
+	Name []VocabularyName `cardinality:"1.." json:"name"           property:"NAME"`
+	Code []Identifier     `cardinality:"0.." json:"code,omitempty" property:"CODE"`
 }
 
 // Language represents a language vocabulary document.
