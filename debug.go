2839fa9332226268bb4d3028e21b45ed17bb0fd7
Mitar <mitar.git@tnode.com>
2026-03-08 13:28:47 +0100

    Expose ES mapping during development.

---
+++ debug.go
@@ -0,0 +1,26 @@
+package peerdb
+
+import (
+	"net/http"
+
+	"gitlab.com/tozd/go/errors"
+	"gitlab.com/tozd/waf"
+
+	"gitlab.com/peerdb/peerdb/internal/mapping"
+)
+
+// DebugMappingGet handles GET requests to serve generated ElasticSearch mapping for debugging.
+func (s *Service) DebugMappingGet(w http.ResponseWriter, req *http.Request, _ waf.Params) {
+	if !s.Development {
+		s.NotFoundWithError(w, req, errors.New("not in development mode"))
+		return
+	}
+
+	indexConfiguration, errE := mapping.Generate()
+	if errE != nil {
+		s.InternalServerErrorWithError(w, req, errE)
+		return
+	}
+
+	s.WriteJSON(w, req, indexConfiguration, nil)
+}
