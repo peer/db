0450a851baa8ee74e13745699f05e132cf625fad
KukovecRok <kukov.rok@gmail.com>
2026-04-01 19:43:50 +0200

    add simple test

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from "@playwright/test"
 
 export default defineConfig({
   testDir: "./tests",
-  testMatch: "*.test.ts",
+  testMatch: "**/*.test.ts",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: 0,
