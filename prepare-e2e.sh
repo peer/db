deff4322f03c708488c7ae49955c9efc72effbf2
KukovecRok <kukov.rok@gmail.com>
2026-04-02 20:19:14 +0200

    remove leftovers

--- prepare-e2e.sh
+++ prepare-e2e.sh
@@ -15,7 +15,7 @@ if [ -n "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" ]; then
     wget -O artifacts.zip "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/jobs/artifacts/refs/merge-requests/${TARGET_MR_IID}/merge/download?job=test_e2e"
   else
     echo "Target branch has no open MR, downloading screenshots from branch artifacts."
-    wget -O artifacts.zip "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/jobs/artifacts/${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}/download?job=test_e2e" || true
+    wget -O artifacts.zip "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/jobs/artifacts/${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}/download?job=test_e2e"
   fi
 elif [ -n "$CI_COMMIT_TAG" ] || [ "$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH" ]; then
   echo "Tag or main branch pipeline, update screenshots."
