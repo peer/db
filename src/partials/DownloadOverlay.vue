742cc8ecc2ec10c49b5d5724e2262cacfb12e855
Mitar <mitar.git@tnode.com>
2026-05-10 00:54:02 +0200

    Smaller gap.

--- src/partials/DownloadOverlay.vue
+++ src/partials/DownloadOverlay.vue
@@ -81,27 +81,29 @@ function onCancel() {
     <!-- Full-screen container to center the panel. -->
     <div class="fixed inset-0 flex items-center justify-center">
       <DialogPanel class="relative flex w-full max-w-md flex-col gap-y-4 rounded-sm bg-white p-4 shadow-sm sm:p-6">
-        <div v-if="downloadingPhase === 'preparing'" class="font-medium">
-          {{ t("partials.DownloadOverlay.preparing") }}
-        </div>
-        <div v-else-if="downloadingPhase === 'downloading'" class="font-medium">
-          {{ t("partials.DownloadOverlay.downloadingFile", { completed: currentIndex, total }) }}
-        </div>
-        <div v-else-if="downloadingPhase === 'empty'" class="font-medium">
-          {{ t("partials.DownloadOverlay.noFiles") }}
-        </div>
+        <div class="flex flex-col gap-y-2">
+          <div v-if="downloadingPhase === 'preparing'" class="font-medium">
+            {{ t("partials.DownloadOverlay.preparing") }}
+          </div>
+          <div v-else-if="downloadingPhase === 'downloading'" class="font-medium">
+            {{ t("partials.DownloadOverlay.downloadingFile", { completed: currentIndex, total }) }}
+          </div>
+          <div v-else-if="downloadingPhase === 'empty'" class="font-medium">
+            {{ t("partials.DownloadOverlay.noFiles") }}
+          </div>
 
-        <div v-if="currentFile" class="truncate text-sm text-neutral-500">{{ currentFile }}</div>
+          <div v-if="currentFile" class="truncate text-sm text-neutral-500">{{ currentFile }}</div>
 
-        <!-- Determinate progress bar. -->
-        <div v-if="showProgress" class="relative h-2 w-full bg-slate-200">
-          <div
-            class="absolute inset-y-0 left-0 bg-secondary-400 transition-all duration-300"
-            :style="{ width: progressPercent + '%', transition: skipTransition ? 'none' : undefined }"
-          />
+          <!-- Determinate progress bar. -->
+          <div v-if="showProgress" class="relative h-2 w-full bg-slate-200">
+            <div
+              class="absolute inset-y-0 left-0 bg-secondary-400 transition-all duration-300"
+              :style="{ width: progressPercent + '%', transition: skipTransition ? 'none' : undefined }"
+            />
+          </div>
+
+          <div v-if="error" class="text-error-600">{{ t("partials.DownloadOverlay.error") }}</div>
         </div>
-
-        <div v-if="error" class="text-error-600">{{ t("partials.DownloadOverlay.error") }}</div>
 
         <div class="flex flex-row justify-end">
           <Button @click="onCancel">{{ closeOnly ? t("common.buttons.close") : t("common.buttons.cancel") }}</Button>
