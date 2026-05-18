838cb698deddd5d5a5aecf090cb55289816dce54
Mitar <mitar.git@tnode.com>
2026-05-18 22:42:16 +0200

    Display intervals.

--- src/partials/claimvalue/ClaimValueTimeInterval.vue
+++ src/partials/claimvalue/ClaimValueTimeInterval.vue
@@ -3,11 +3,25 @@ import type { DeepReadonly } from "vue"
 
 import type { TimeIntervalClaim } from "@/document"
 
+import { useI18n } from "vue-i18n"
+
+import TimeDisplay from "@/partials/TimeDisplay.vue"
+
 defineProps<{
   claim: TimeIntervalClaim | DeepReadonly<TimeIntervalClaim> | null
 }>()
+
+const { t } = useI18n({ useScope: "global" })
 </script>
 
 <template>
-  <template v-if="claim">{{ claim.from ?? "?" }}–{{ claim.to ?? "?" }}</template>
+  <template v-if="claim">
+    <TimeDisplay v-if="claim.from" :timestamp="claim.from" :precision="claim.fromPrecision!" />
+    <template v-else-if="claim.fromIsUnknown">{{ t("common.values.unknown") }}</template>
+    <template v-else-if="claim.fromIsNone">{{ t("common.values.none") }}</template>
+    –
+    <TimeDisplay v-if="claim.to" :timestamp="claim.to" :precision="claim.toPrecision!" />
+    <template v-else-if="claim.toIsUnknown">{{ t("common.values.unknown") }}</template>
+    <template v-else-if="claim.toIsNone">{{ t("common.values.none") }}</template>
+  </template>
 </template>
