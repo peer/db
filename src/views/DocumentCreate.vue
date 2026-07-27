d057591442a1ca2f0171ef6b882d0d2ebcbcbd9d
Mitar <mitar.git@tnode.com>
2026-07-27 18:53:32 +0200

    Style.

--- src/views/DocumentCreate.vue
+++ src/views/DocumentCreate.vue
@@ -209,11 +209,13 @@ async function onCreate(classId: string, replace = false) {
     <NavBar />
   </Teleport>
   <div class="pd-documentcreate mt-[var(--pd-navbar-offset)] flex w-full flex-col p-1 sm:p-4 xl:px-16">
-    <div v-if="!loaded" class="my-1 sm:my-4">{{ t("common.status.loading") }}</div>
-    <div v-else-if="tree.length === 0" class="my-1 sm:my-4">{{ t("views.DocumentCreate.noClasses") }}</div>
-    <div v-else class="flex w-full flex-col gap-y-2 sm:gap-y-4">
-      <h1 class="text-3xl font-bold drop-shadow-xs">{{ t("views.DocumentCreate.title") }}</h1>
-      <ClassTreeList :nodes="tree" :on-create="onCreate" />
+    <div class="flex flex-col rounded-sm border border-gray-200 bg-white p-4 shadow-sm">
+      <div v-if="!loaded" class="my-1 sm:my-4">{{ t("common.status.loading") }}</div>
+      <div v-else-if="tree.length === 0" class="my-1 sm:my-4">{{ t("views.DocumentCreate.noClasses") }}</div>
+      <div v-else class="flex w-full flex-col gap-y-2 sm:gap-y-4">
+        <h1 class="text-3xl font-bold drop-shadow-xs">{{ t("views.DocumentCreate.title") }}</h1>
+        <ClassTreeList :nodes="tree" :on-create="onCreate" />
+      </div>
     </div>
   </div>
   <Teleport to="footer">
